d8cc45257d34240eb2e330c000c1e694f2c3df58
Kai Richard Koenig <kai.richard.koenig@googlemail.com>
2014-03-27 17:04:34 +0100

    Echo composer now can echo to a file

--- lib/zq/composers/echo.rb
+++ lib/zq/composers/echo.rb
@@ -1,13 +1,16 @@
 module ZQ
   module Composer
     class NoOp
-      def compose data, composite = nil
+      def compose(data, composite = nil)
         data
       end
     end
     class Echo
-      def compose data, composite = nil
-        puts data
+      def initialize(ioo = nil)
+        @file = ioo || $stdout
+      end
+      def compose(data, composite = nil)
+        @file.puts data
       end
     end
   end
--- tests/composer_test.rb
+++ tests/composer_test.rb
@@ -1,12 +1,26 @@
 require 'test_helper'
 
 class EchoComposerTestCase < ZQTestCase
-  def test_compose
-    composer = ZQ::Composer::Echo.new
+  def test_compose_stdout_implicit
     test_data = "a"
     assert_output test_data + "\n" do
+      composer = ZQ::Composer::Echo.new
       composer.compose(test_data)
     end
+  end
+  def test_compose_stdout_explicit
+    test_data = "a"
+    assert_output test_data + "\n" do
+      composer = ZQ::Composer::Echo.new $stdout
+      composer.compose(test_data)
+    end
+  end
+  def test_compose_to_file
+    test_data = "a"
+    file = StringIO.new
+    composer = ZQ::Composer::Echo.new file
+    composer.compose(test_data)
+    assert_equal(test_data + "\n", file.string)
   end
 end
 
